999e838219cd238b50960192756aa7cc116280e7
Andrew Lee <leezandrew@gmail.com>
2020-01-20 18:44:09 -0500

    added prepare_db.py to manage db

---
+++ prepare_db.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+"""
+CREATE TABLE "sponsorTimes" (
+        "videoID"       TEXT NOT NULL,
+        "startTime"     REAL NOT NULL,
+        "endTime"       REAL NOT NULL,
+        "votes" INTEGER NOT NULL,
+        "UUID"  TEXT NOT NULL UNIQUE,
+        "userID"        TEXT NOT NULL,
+        "timeSubmitted" INTEGER NOT NULL,
+        "views" INTEGER NOT NULL,
+        "shadowHidden"  INTEGER NOT NULL
+)
+"""
+
+def createSponsor():
+    conn = sqlite3.connect(r"C:\Users\Andrew\Documents\NeuralBlock\data\labeled.db")
+    cursor = conn.cursor()
+    query = """
+    CREATE TABLE "SponsorData" (
+            "videoID"       TEXT NOT NULL,
+            "startTime"     REAL NOT NULL,
+            "endTime"       REAL NOT NULL,
+            "votes" INTEGER NOT NULL,
+            "text" BLOB,
+            "processed" INTEGER NOT NULL
+    )
+    """
+    cursor.execute(query)
+    conn.commit()
+    conn.close()
+    return
+
+def createRandom():
+    conn = sqlite3.connect(r"C:\Users\Andrew\Documents\NeuralBlock\data\labeled.db")
+    cursor = conn.cursor()
+    query2 = """
+    CREATE TABLE "RandomData" (
+            "videoID"       TEXT NOT NULL,
+            "startTime"     REAL NOT NULL,
+            "endTime"       REAL NOT NULL,
+            "text" BLOB
+    )
+    """
+    cursor.execute(query2)
+    conn.commit()
+    conn.close()
+    return
+
+def truncateTable(table):
+    conn = sqlite3.connect(r"C:\Users\Andrew\Documents\NeuralBlock\data\labeled.db")
+    cursor = conn.cursor()
+    cursor.execute(f"delete from {table}")
+    conn.commit()
+    cursor.execute("vacuum")
+    conn.close()
+    return
+
+
+#truncateTable("sponsordata")
+#truncateTable("randomdata")
