c77456c8de68c282114912a0c5777c13fef94a59
Andrew Lee <leezandrew@gmail.com>
2020-03-08 20:09:23 -0400

    Model files to run NN released

--- README.md
+++ README.md
@@ -3,9 +3,17 @@ NeuralBlock is a neural network built using Keras/Tensorflow that detects in-vid
 
 This project is loosely based on and inspired by the project https://github.com/Sponsoff/sponsorship_remover. Unlike the aforementioned project, this project leverages the crowd-sourced labels of https://github.com/ajayyy/SponsorBlock.
 
-# High Level Summary
+## High Level Summary
 1. NeuralBlock extract transcripts from youtube videos via https://pypi.org/project/youtube-transcript-api/ python library.
 2. The SponsorBlock community has already pre-labeled sponsors.
 3. The timestamps from (2) are used to label the transcript word for word, thereby creating a training set.
 4. The sequence of text is tokenized using the top 10,000 words found in sponsorships. A word embedding using fastText is generated.
 5. Finally, a bidirectional LSTM is trained. The network outputs, at every word, a probability whether or not this word is part of a sponsorship.
+
+##Predicting On New Data
+1. Install the python libraries tensorflow and YouTubeTranscriptApi
+2. Update paths if necessary
+3. Provide a video id (vid). The network was trained on the database as of 3/3/20. Use a video that was created after that date to ensure that the video hasn't already been seen.
+4. Run predict_stream.py
+5. Manually inspect the output stored in "df". 
+
--- predict_stream.py
+++ predict_stream.py
@@ -3,13 +3,14 @@ import json
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
 from youtube_transcript_api import YouTubeTranscriptApi
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pandas as pd
 
-model = load_model("./data/models/nb_stream_5.h5")
+model = load_model("./data/models/nb_stream_fasttext_10k.h5")
 with open("./data/tokenizer_stream_10k.json") as f:
     json_obj = json.load(f)
     tokenizer = tokenizer_from_json(json_obj)
 
-vid = "6hJ_E2NOdcw"
+vid = "JbdGVtj4whA"
 
 transcript = YouTubeTranscriptApi.get_transcript(vid, languages = ["en"])
 
@@ -18,8 +19,17 @@ for i in transcript:
     full_text = full_text + " " + i["text"]
 
 full_text = full_text.replace("\n", " ")
-print(full_text)
 full_seq = tokenizer.texts_to_sequences([full_text])
-print(len(full_seq[0]))
-full_seq = pad_sequences(full_seq, maxlen = 3000, padding = "post")
-results = model.predict(full_seq, batch_size = 1).round(3)
+seqlen = len(full_seq[0])
+print("Sequence length: {}".format(seqlen))
+#Videos with more than 3000 words need to be split up.
+maxseqlen = 3000
+if seqlen <= maxseqlen:
+    print(full_text)
+    full_seq = pad_sequences(full_seq, maxlen = 3000, padding = "post")
+    full_text = tokenizer.sequences_to_texts(full_seq)[0].split(" ")
+    results = model.predict(full_seq, batch_size = 1).round(3)
+    df = pd.DataFrame(results[0,:,:])
+    df["text"] = full_text
+else:
+    print("Video needs to be 3000 words or fewer.")
