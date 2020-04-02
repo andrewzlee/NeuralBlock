78be56386dce60e2bc44ba4308176913c9decf41
Andrew Lee <leezandrew@gmail.com>
2020-04-02 18:49:46 -0400

    Updated predict_stream.py

--- predict_stream.py
+++ predict_stream.py
@@ -2,35 +2,45 @@ from tensorflow.keras.models import load_model
 import json
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
 from youtube_transcript_api import YouTubeTranscriptApi
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import re
 import pandas as pd
+import app.algorithms.process_predictions as pp
+
+MAXWORDS = 3000
 
 model = load_model("./data/models/nb_stream_fasttext_10k.h5")
 with open("./data/tokenizer_stream_10k.json") as f:
     json_obj = json.load(f)
     tokenizer = tokenizer_from_json(json_obj)
 
-vid = "aFyrgaC8iB4"
+vid = "QqEuO5im1nA"
+channel = "HalfAsInteresting" #For file naming only
 
 transcript = YouTubeTranscriptApi.get_transcript(vid, languages = ["en"])
 
+chars = "(!|\"|#|\$|%|&|\(|\)|\*|\+|,|-|\.|/|:|;\<|=|>|\?|@|\[|\\\\|\]|\^|_|`|\{|\||\}|~|\t|\n)+"
+captionCount = []
 full_text = ""
-for i in transcript:
-    full_text = full_text + " " + i["text"]
+for t in transcript:
+    cleaned_text = re.sub("  +", " ", re.sub(chars, " ", t["text"])).strip()
+    captionCount.append(len(cleaned_text.split(" ")))
+    full_text = full_text + " " + cleaned_text
+full_text = full_text.strip()
 
-full_text = full_text.replace("\n", " ")
-full_seq = tokenizer.texts_to_sequences([full_text])
-seqlen = len(full_seq[0])
-print("Sequence length: {}".format(seqlen))
-#Videos with more than 3000 words need to be split up.
-maxseqlen = 3000
-if seqlen <= maxseqlen:
-    print(full_text)
-    full_seq = pad_sequences(full_seq, maxlen = maxseqlen, padding = "post")
-    full_text = tokenizer.sequences_to_texts(full_seq)[0].split(" ")
-    results = model.predict(full_seq, batch_size = 1).round(3)
-    df = pd.DataFrame(results[0,:,:])
-    df["text"] = full_text
-    df.to_csv(f"./examples/{vid}.csv", index = False)
+predictions, status = pp.getPredictions(model,tokenizer,full_text)
+
+if status:
+    df = pd.DataFrame(predictions[0,:,:])
+    words = full_text.split(" ")
+    df["text"] =  words + ["N/A"]*(MAXWORDS-len(words))
+    df.to_csv(f"./examples/{channel}_{vid}.csv", index = False)
+
+    sponsorTimestamps,sponsorText = pp.getTimestamps(transcript, captionCount, predictions[0], words)
+    print(sponsorTimestamps)     
+   
+    with open(f"./examples/{channel}_{vid}.txt", 'w') as file:
+        file.write("Timestamps:\n")
+        for ts in sponsorTimestamps:
+            file.write('%s\n' % str(ts))
 else:
-    print("Video needs to be 3000 words or fewer.")
+    print("Failed")
