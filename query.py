5b57c23eea9cc2a41b8000ecbebe653534585429
Andrew Lee <leezandrew@gmail.com>
2020-07-24 21:42:43 -0400

    Added test queries to query.py

--- query.py
+++ query.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+import random
 
 
-
-#conn_src = sqlite3.connect(r"./data/database.db")
+conn_src = sqlite3.connect(r"./data/database.db")
 conn_dest = sqlite3.connect(r"./data/labeled.db")
 
 
-#cursor_src = conn_src.cursor()
+cursor_src = conn_src.cursor()
 cursor_dest = conn_dest.cursor()
 
 
-query = "select * from sponsordata"
+query = "select processed, autogen, count(distinct videoid) from sponsordata group by processed, autogen"
+query = "select videoid from sponsordata where sponsor = 1 group by videoid, starttime, endtime having count(*) > 1"
+query = "select * from sponsordata where sponsor = 1 and length(text) = 0"
+#query = "select count(distinct videoid) from sponsortimes where votes > 1"
+#query = "delete from sponsordata where videoid = 'xy-RUYg-xuQ'"
 
 cursor_dest.execute(query)
+#conn_dest.commit()
 res = cursor_dest.fetchall()
 
 for r in res:
     print(r)
     
-
+conn_src.close()
 conn_dest.close()
