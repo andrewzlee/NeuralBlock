5d94a69f62b90e44fc844e92fd6e89ebcc031bc9
Andrew Lee <leezandrew@gmail.com>
2020-08-28 12:11:02 -0400

    Add random segment to predict on

--- predict.py
+++ predict.py
@@ -1,3 +1,6 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
 import requests
 import pandas as pd
 import json
@@ -5,13 +8,14 @@ from tensorflow.keras.preprocessing.text import tokenizer_from_json
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from youtube_transcript_api import YouTubeTranscriptApi
+import random
 
 #import sys
 #sys.path.append("app/algorithms")
 import app.algorithms.process_predictions as process #Custom script to extract data
 
 #Video ID to predict on
-vid = "Gnhv3WFnmH8"
+vid = "46gNvDLgLdI"
 
 model = load_model("./data/models/nb_spot.h5")
 with open("./data/spot_tokenizer_10k.json") as f:
@@ -21,10 +25,34 @@ with open("./data/spot_tokenizer_10k.json") as f:
 transcript = YouTubeTranscriptApi.get_transcript(vid, languages = ["en"])
 segments = requests.get("https://sponsor.ajay.app/api/skipSegments",params={"videoID":vid}).json()
 
+#Sponsored Segments
 text = []
 for seg in segments:
     string = process.extractText(seg["segment"],transcript, widen = 0.05)[0]
-    print("Predicting on <<" + string[0:100] + "...>>")
+    print("[SPONSOR]:: <<" + string + ">>")
+    text.append(string)
+
+#Random 10 second segment
+retry = 1
+counter = 0
+while retry:
+    start_point = random.sample(transcript,1)[0]["start"]
+    retry = 0
+    counter += 1
+    #Quit after 20 tries
+    if counter >= 20:
+        print("[NON-SPONSOR]:: NONE FOUND")
+        break
+
+    for seg in segments:
+        #Check to ensure it's not in a sponsorship
+        if seg["segment"][0] <= start_point <= seg["segment"][1]:
+            retry = 1
+
+if counter < 20:
+    #Extract text
+    string = process.extractText((start_point, start_point+10),transcript, widen = 0.05)[0]
+    print("[NON-SPONSOR]:: <<" + string + ">>")
     text.append(string)
 
 data = pd.DataFrame({"text":text})
