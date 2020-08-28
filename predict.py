c5b393d2bf597c9ed0494b85177bf86589631acb
Andrew Lee <leezandrew@gmail.com>
2020-08-27 22:13:51 -0400

    Refreshed predict.py

--- predict.py
+++ predict.py
@@ -1,32 +1,34 @@
+import requests
 import pandas as pd
 import json
-from keras.preprocessing.text import tokenizer_from_json
-from keras.models import load_model
-from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from youtube_transcript_api import YouTubeTranscriptApi
 
-model = load_model("./data/nb_model.h5")
-with open("./data/tokenizer.json") as f:
+#import sys
+#sys.path.append("app/algorithms")
+import app.algorithms.process_predictions as process #Custom script to extract data
+
+#Video ID to predict on
+vid = "Gnhv3WFnmH8"
+
+model = load_model("./data/models/nb_spot.h5")
+with open("./data/spot_tokenizer_10k.json") as f:
     json_obj = json.load(f)
     tokenizer = tokenizer_from_json(json_obj)
 
-vid = "N1rTDbjc_fI"
-
 transcript = YouTubeTranscriptApi.get_transcript(vid, languages = ["en"])
+segments = requests.get("https://sponsor.ajay.app/api/skipSegments",params={"videoID":vid}).json()
 
-text1 = ""
-for i in range(0,3):
-    text1 = text1 + " " + transcript[i]["text"]
-    
-text2 = ""
-for i in range(24,39):
-    text2 = text2 + " " + transcript[i]["text"]
-    
-text3 = ""
-for i in range(76,88):
-    text3 = text3 + " " + transcript[i]["text"]
+text = []
+for seg in segments:
+    string = process.extractText(seg["segment"],transcript, widen = 0.05)[0]
+    print("Predicting on <<" + string[0:100] + "...>>")
+    text.append(string)
 
-dfnew = pd.DataFrame({"text":[text1,text2,text3]})
-x_new = tokenizer.texts_to_sequences(dfnew["text"].values)
-x_new = pad_sequences(x_new, maxlen=3943)
+data = pd.DataFrame({"text":text})
+x_new = tokenizer.texts_to_sequences(data["text"].values)
+x_new = pad_sequences(x_new, padding = "post", maxlen = 3000, truncating = "post")
+
 print(model.predict(x_new, batch_size = 1).round(3))
